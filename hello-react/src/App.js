47b2ab6ec2bcc6ad1bc7bb390327d2893b5e4ad3
blacktoast <dbstjd100@naver.com>
2022-01-06 08:51:35 +0000

    useRef + 배열렌더링

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,10 +1,67 @@
 import "./App.css";
 
-import React from "react";
+import React, { useRef, useState } from "react";
 import InputSample from "./inputSample";
+import UserList from "./components/UserList";
+import CreateUser from "./components/CreateUser";
 
 const App = () => {
-  return <InputSample></InputSample>;
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+  });
+  const { username, email } = inputs;
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: "velopert",
+      email: "public.velopert@gmail.com",
+    },
+    {
+      id: 2,
+      username: "tester",
+      email: "tester@example.com",
+    },
+    {
+      id: 3,
+      username: "liz",
+      email: "liz@example.com",
+    },
+  ]);
+
+  const nextId = useRef(4);
+
+  const onCreate = () => {
+    // 나중에 구현 할 배열에 항목 추가하는 로직
+    // ...
+
+    setInputs({
+      username: "",
+      email: "",
+    });
+    nextId.current += 1;
+  };
+
+  return (
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} />;
+    </>
+  );
 };
 
 export default App;
